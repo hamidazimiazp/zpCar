66ef7cb3f89e7c1b1b814849a927587a905904bb
hamidazimiazp <hamidazimi.azp@gmail.com>
2023-03-01 14:51:03 +0330

    update global styles

--- app/src/components/layout/Layout.js
+++ app/src/components/layout/Layout.js
@@ -2,10 +2,11 @@ import React from "react";
 import Header from "../modules/Header/Header";
 import Footer from "../modules/Footer/Footer";
 
-const Layout = () => {
+const Layout = ({ children }) => {
   return (
     <div className="root">
       <Header />
+      <main>{children}</main>
       <Footer />
     </div>
   );
