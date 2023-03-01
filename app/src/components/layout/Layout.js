270ba5cdf4173b6956f1c4358e32d3fc058e6a8b
hamidazimiazp <hamidazimi.azp@gmail.com>
2023-03-01 14:47:37 +0330

    add footer

--- app/src/components/layout/Layout.js
+++ app/src/components/layout/Layout.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../modules/Header/Header";
+import Footer from "../modules/Footer/Footer";
 
 const Layout = () => {
   return (
     <div className="root">
       <Header />
+      <Footer />
     </div>
   );
 };
