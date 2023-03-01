60c1d42afb74551878193801f2cbd901930dd31e
hamidazimiazp <hamidazimi.azp@gmail.com>
2023-03-01 14:40:13 +0330

    add header

---
+++ app/src/components/layout/Layout.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../modules/Header/Header";
+
+const Layout = () => {
+  return (
+    <div className="root">
+      <Header />
+    </div>
+  );
+};
+
+export default Layout;
