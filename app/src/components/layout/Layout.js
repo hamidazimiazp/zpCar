4c9ea463b7d9de517538ec739e59d01ddadacbcd
hamidazimiazp <hamidazimi.azp@gmail.com>
2023-03-01 14:55:18 +0330

    make grid for project

--- app/src/components/layout/Layout.js
+++ app/src/components/layout/Layout.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Header from "../modules/Header/Header";
 import Footer from "../modules/Footer/Footer";
+import styles from "./Layout.module.css";
 
 const Layout = ({ children }) => {
   return (
-    <div className="root">
+    <div className={styles.wrapper}>
       <Header />
-      <main>{children}</main>
+      <main className={styles.main}>{children}</main>
       <Footer />
     </div>
   );
