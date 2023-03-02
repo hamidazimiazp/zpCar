7d0ce23620e5d7927418be6454cad1478d97adb7
hamidazimiazp <hamidazimi.azp@gmail.com>
2023-03-02 09:31:43 +0330

    add back button to car list page

--- app/src/pages/cars/index.js
+++ app/src/pages/cars/index.js
@@ -1,6 +1,10 @@
+import Button from "@/components/modules/Button/Button";
 import CarList from "@/components/templates/carList/carList";
 import getCars from "@/data/carsData";
+import { useRouter } from "next/router";
 import React, { useState, useEffect } from "react";
+import { MdKeyboardArrowLeft } from "react-icons/md";
+import styles from "./cars.module.css";
 
 const index = () => {
   const [cars, setCars] = useState([]);
@@ -9,8 +13,24 @@ const index = () => {
     setCars(getCars());
   }, []);
 
+  const router = useRouter();
+
+  const backHandler = () => {
+    router.back();
+  };
+
   return (
     <div>
+      <div className={styles.backButton}>
+        <Button
+          text="Back"
+          color="#fff"
+          bgColor="#262525"
+          clicked={backHandler}
+          icon={<MdKeyboardArrowLeft color="#fff" size={18} />}
+        />
+      </div>
+
       <CarList data={cars} />
     </div>
   );
